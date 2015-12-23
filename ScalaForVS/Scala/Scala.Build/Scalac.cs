b8204a367941bbb6bb0a018ca693e8f4755b88ad
Michael <b.michael.dick@gmail.com>
2015-12-23 10:53:20 -0800

    This is not the commit message you are looking for

---
+++ ScalaForVS/Scala/Scala.Build/Scalac.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Scala.Build
+{
+    public class Scalac : Task
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScalaForVS/Scala/Scala.ProjectTemplate/Program.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by mdick on 12/22/2015.
+  */
+
+object ScalaApp {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
